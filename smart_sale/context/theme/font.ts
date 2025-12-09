a339909e1de72a5eac26e7b99406b5a5ffcfe00b
aswinkumar1126 <144091947+aswinkumar1126@users.noreply.github.com>
2025-12-09 10:51:57 +0530

    initial commit
    initial commit for login

---
+++ smart_sale/context/theme/font.ts
@@ -0,0 +1,46 @@
+import {
+    Alice,
+    Nosifer,
+    Lustria,
+    Rancho,
+    Sofia,
+} from "next/font/google";
+
+// Primary font
+export const alice = Alice({
+    subsets: ["latin"],
+    variable: "--font-alice",
+    weight: "400",
+});
+
+// Heading font
+export const nosifer = Nosifer({
+    subsets: ["latin"],
+    variable: "--font-nosifer",
+    weight: "400",
+});
+
+// Body font
+export const lustria = Lustria({
+    subsets: ["latin"],
+    variable: "--font-lustria",
+    weight: "400",
+});
+
+// Special
+export const rancho = Rancho({
+    subsets: ["latin"],
+    variable: "--font-rancho",
+    weight: "400",
+});
+
+// Secondary
+export const sofia = Sofia({
+    subsets: ["latin"],
+    variable: "--font-sofia",
+    weight: "400",
+});
+
+// Export ALL in one class string
+export const fontVariables =
+    `${alice.variable} ${nosifer.variable} ${lustria.variable} ${rancho.variable} ${sofia.variable}`;
