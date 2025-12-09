ffd9d57625ca2189eafc26912ffee49090e8e2e5
Aswinkumar <144091947+aswinkumar1126@users.noreply.github.com>
2025-12-09 10:53:10 +0530

    Revert "initial commit"

--- smart_sale/app/page.tsx
+++ smart_sale/app/page.tsx
@@ -1,11 +1,8 @@
-"use client"
 import Image from "next/image";
-import Loader from "@/component/loader/Loader";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Loader isLoading={true} fullscreen={true}/>
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
